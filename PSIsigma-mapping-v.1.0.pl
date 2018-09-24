a7c31bf2b4db1c260572b4ddef5b9797adea6130
Woody Lin <woodydon777@gmail.com>
2018-09-24 16:44:13 -0400

    Delete PSIsigma-mapping-v.1.0.pl

--- PSIsigma-mapping-v.1.0.pl
+++
@@ -1,9 +0,0 @@
-=begin
-PSI-Sigma: A splicing-detection method for short-read and long-read RNA-seq data
-© Kuan-Ting Lin, 2018-2024
-PSI-Sigma is free for non-commercial purposes by individuals at an academic or non-profit institution.
-For commercial purposes, please contact tech transfer office of CSHL via narayan@cshl.edu
-=end
-=cut
-#!/usr/bin/perl -w
-eval unpack u=>q{_=7-E('-T<FEC=#MM>2@D9W1F*3U`05)'5CMM>25A;FYO.V]P96XH1DE,12PB)&=T9B(I?'QD:64B06)O<G1I_;F<N+B!#86XG="!O<&5N("1G=&8@.B`D(5QN(CMW:&EL92AM>21L:6YE/3Q&24Q%/BE[8VAO;7`D;&EN93MN_97AT(&EF*"1L:6YE/7XO7EPC+RD[;7E`87)R87D]<W!L:70H+UQT+RPD;&EN92D[;F5X="!I9B@D87)R87E;_,EUN92)T<F%N<V-R:7!T(BD[;7DH)&-H<BPD<W1A<G0L)&5N9"PD<W1R86YD+"1N86UE*3TH)&%R<F%Y6S!=_+"1A<G)A>5LS72PD87)R87E;-%TL)&%R<F%Y6S9=+"1A<G)A>5LX72D[)&-H<CTB8VAR(BXD8VAR(&EF*"1C_:'(A?B]C:'(O*3LD8VAR/7YS+V-H<DU4+V-H<DTO.VUY)&ED/20Q(&EF*"1N86UE/7XO=')A;G-C<FEP=%Q?_:60@7#Q?7U-44DE.1U]?/EP[+RD[;7DD9VX])&YA;64[)&=N/7YS+R@N*BEG96YE7%]N86UE7"(H7'<K*5PB_7#LH+BHI+R\[)&=N/7YS+V=E;F5<7VED7"(O+SL*("`@("`@("`D9VX]?G,O7")#86XB+R\["B`@("`@("`@_:68H(21G;BE["B`@("`@("`@"7!R:6YT(")T(&]P96XD;W5T9FY<;B=T(&9I;F0@9V5N92!S>6UB;VP@9F]R_(&ED(#T@)&ED7&XB.PH@("`@("`@(`EE>&ET.PH@("`@("`@('T*("`@("`@("`D86YN;WLB)&ED(GT@/2`B_)&=N7'0D<W1R86YD(CL*("`@('T*("`@(&-L;W-E*$9)3$4I.PH@("`@"B`@("!M>2`D;W5T9FX@/2`D9W1F_.PH@("`@)&]U=&9N/7YS+UPN9W1F+UPN4%-)+7-I9VUA7"YM87!P:6YG7"YT>'0O.PH@("`@;W!E;BA/550L_(CXD;W5T9FXB*2!\?"!D:64@(D%B;W)T:6YG+BX@0V%N)R(["@EF;W)E86-H(&UY("1I9"AS;W)T(&ME>7,@_)6%N;F\I>PH@("`@("`@(&YE>'0@:68H)&ED(&5Q("(D:60@:&%S(&YO(&UA<'!I;F<@<F5S=6QT<RY<;B(I_.PH@("`@("`@(&EF*"$D86YN;WLD:61]*7L*("`@("`@("`)<')I;G0@(B1I9%QT(CL*("`@("`@("!]"B`@?("`@("`@<')I;G0@3U54(")<;B)]8VQO<V4H3U54*0}
