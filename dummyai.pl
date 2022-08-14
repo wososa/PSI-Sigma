d13c630be626bea9e448c0c14d8256cac972d275
Woody Lin <woodydon777@gmail.com>
2022-08-14 10:25:12 -0400

    Add files via upload
    version 2.0 improvements.

--- dummyai.pl
+++ dummyai.pl
@@ -10,18 +10,23 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	use Cwd qw(abs_path);
 	
 	my $status = param(\@ARGV);
+	my $inputs = "";
+	foreach my $in(@ARGV){
+		$inputs .= " " . $in;
+	}
 	
 	my @param = split(/\t/,$status);
 	
 	if($param[0] eq "Help" || scalar @param < 4){
 		print "\n";
-		print "Usage: perl dummyai.pl [parameters]\n";
+		print "Usage: perl dummyai-parallel.pl [parameters]\n";
 		print "Parameters:\n";
 		print "  --gtf [text]		the gene annotation file for building PSI-Sigma database.\n";
 		print "  --name [text]		the prefix of PSI-Sigma database and output files.\n";
 		print "  --type [number]	1: short-read RNA-seq data\n";
 		print "			2: long-read RNA-seq data\n";
 		print "  --nread [number]	the minimum number of supporting reads for a splicing event.\n";
+		print "  --output [/path]	Publish results to the designated folder.  (default: current folder)\n";
 		print "  --skipratio [number]	the ratio (0~1) of skipping reads in a exon-skipping event [default: 0.05]. A splicing event has to have at least one input sample passing this criteria to be included in the analysis. \n";
 		print "  --fmode [number]	0: delta-PSI > 10% and p-value < 0.01 (default/recommended)\n";
 		print "			1: delta-PSI > 10%\n";
@@ -30,21 +35,28 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 		print "  --irmode [number]	0: only introns in the .gtf. (default)\n";
 		print "			1: aggressively search for all introns\n";
 		print "			2: Skip intron-retention events.\n";
+		print "  --irrange [number]	0: IR event is using reference points of the target exon region.\n";
+		print "			n: Use +-n bases around target intron region and +-n bases around event region. (default)\n";
+		print "  --irclean [number]	0: No action (default)\n";
+		print "			25~100: Apply skipratio and remove intron-retention events whose coverage difference between the its two splice sites is > n% of their average.\n";
 		print "  --adjp [number]	0: Skip p-value adjustment. (default)\n";
 		print "			1: Benjamini-Hochberg (Statistics::Multtest Perl module)\n";
 		print "			2: Benjamini-Hochberg (qvalue or p.adjust() R package)\n";
 		print "  --trimp [number]	Set FDR to 'na' for events whose maximum PSI values are below [number]% or whose minimum PSI values are above [100-number]% in all groupa.txt and groupb.txt during p-value adjustment. [default:5]\n";
 		print "  --denominator [number]	0: Don't report denominators. (default)\n";
 		print "			1: Report the table of denominators.\n";
-		print "  --irrange [number]	0: IR event is using reference points of the target exon region.\n";
-		print "			n: Use +-n bases around target intron region and +-n bases around event region. (default)\n";
 		print "  --variance [number]	0: Assuming equal variance and use Student's t-test. (default)\n";
 		print "			1: Assuming unequal variance and use Welch's t-test. \n";
+		print "  --groupa [/path/groupa.txt]	Specify where the groupa.txt file is. (default: groupa.txt)\n";
+		print "  --groupb [/path/groupb.txt]	Specify where the groupb.txt file is. (default: groupb.txt)\n";
+		print "  --threads [number]	the number of CPUs used when --irmode > 0.\n";
+		print "			1: one CPU thread (4~8GB RAM per CPU thread)\n";
+		print "			1+: more CPU thread (2 = 8~16GB RAM, 3 = 12~24GB RAM, and so forth.)\n";
 		print "\n";
 		exit;
 	}
 	
-	my ($gtf,$name,$type,$supporting_read_criteria,$fmode,$skipratio,$irmode,$adjp,$trimp,$denominator,$irrange,$variance) = split(/\t/,$status);
+	my ($gtf,$name,$type,$supporting_read_criteria,$fmode,$skipratio,$irmode,$adjp,$trimp,$denominator,$irrange,$variance,$threads,$groupa,$groupb,$output,$irclean) = split(/\t/,$status);
 	$fmode = 0 if($fmode ne "0" && $fmode ne "1" && $fmode ne "2" && $fmode ne "3");
 	$irmode = 0 if($irmode ne "0" && $irmode ne "1" && $irmode ne "2");
 	$irrange = 5 if(!$irrange || $irrange!~/\d/ || $irrange eq "-");
@@ -54,6 +66,48 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	$skipratio = 0.05 if($skipratio eq "-" || $skipratio > 1 || $skipratio < 0);
 	$trimp = 5 if($trimp eq "-");
 	$variance = 0 if(!$variance);
+	$threads = 1 if(!$threads);
+	$groupa = "groupa.txt" if(!$groupa);
+	$groupb = "groupb.txt" if(!$groupb);
+	$output = "." if(!$output);
+	$irclean = 0 if(!$irclean);
+	
+	if(!-e $groupa){
+		print "$groupa is not found. exit.\n";
+		exit;
+	}else{
+		print "$groupa is found. Copy to the working directory.\n";
+		system("cp $groupa $name.groupa.txt");
+		$groupa = $name . ".groupa.txt";
+	}
+	if(!-e $groupb){
+		print "$groupb is not found. exit.\n";
+		exit;
+	}else{
+		print "$groupb is found. Copy to the working directory.\n";
+		system("cp $groupb $name.groupb.txt");
+		$groupb = $name . ".groupb.txt";
+	}
+	
+	if($irclean != 0){
+		if($irclean < 25 || $irclean > 100){
+			print "(ERROR) --irclean parameter should in a range of 25% to 100%.\n";
+			exit;
+		}
+	}
+	
+	if($output ne "."){
+		if(-e $output){
+			print "$output folder already exists. exit.\n";
+			exit;
+		}else{
+			system("mkdir $output");
+		}
+		if(!-e $output){
+			print "$output can't be created. exit.\n";
+			exit;
+		}
+	}
 	
 	print "gtf = $gtf\n";
 	print "name = $name\n";
@@ -66,6 +120,9 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	print "trimp = $trimp\n";
 	print "denominator = $denominator\n";
 	print "irrange = $irrange\n";
+	print "groupa = $groupa\n";
+	print "groupb = $groupb\n";
+	
 	if($variance == 0){
 		print "variance assumption = equal (Student's t-test)\n";
 	}
@@ -81,7 +138,12 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	
 	my $path = abs_path($0);
 	$path=~s/\/dummyai\.pl//;
+	$path=~s/\/dummyai\-parallel\.pl//;
 	print "Path = $path\n";
+	if($path=~/\.pl$/){
+		print "Path format isn't correct.\n";
+		exit;
+	}
 	
     my ($starttime,$stoptime,$hours) = (time,0,0);
 
@@ -92,13 +154,15 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	
     my %group;
     my $patha = 0;
- 	open(FILE,"groupa.txt") || die "Aborting.. Can't open groupa.txt : $!\n";
+ 	open(FILE,"$groupa") || die "Aborting.. Can't open $groupa : $!\n";
     while(my $line=<FILE>){
         chomp $line;
         next if($line eq "");
         my $bam = $line;
         my $bamfn = $bam;
         my $bai = "$bam\.bai";
+        my $sjout = $bam;
+        $sjout=~s/\.Aligned\.sortedByCoord\.out\.bam/\.SJ\.out\.tab/;
         my $tmp = "-";
         if($bam!~/\.bam/){
         	$tmp = $bam . ".Aligned.sortedByCoord.out.bam";
@@ -113,6 +177,11 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
         	$bamfn=~s/(.*)\///;
         	system("ln -s $bam $bamfn");
         	system("ln -s $bam\.bai $bamfn\.bai");
+        	if(-e $sjout){
+        		system("cp $sjout .");
+        	}else{
+        		print "(WARNNING) $sjout is not found.\n";
+        	}
         	$bam = $bamfn;
         }
         if($bam=~/\.bam$/){
@@ -128,13 +197,15 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
         $group{$bam}++;
     }
     
- 	open(FILE,"groupb.txt") || die "Aborting.. Can't open groupb.txt : $!\n";
+ 	open(FILE,"$groupb") || die "Aborting.. Can't open $groupb : $!\n";
     while(my $line=<FILE>){
         chomp $line;
         next if($line eq "");
         my $bam = $line;
         my $bamfn = $bam;
         my $bai = "$bam\.bai";
+        my $sjout = $bam;
+        $sjout=~s/\.Aligned\.sortedByCoord\.out\.bam/\.SJ\.out\.tab/;
         my $tmp = "-";
         if($bam!~/\.bam/){
         	$tmp = $bam . ".Aligned.sortedByCoord.out.bam";
@@ -149,6 +220,11 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
         	$bamfn=~s/(.*)\///;
         	system("ln -s $bam $bamfn");
         	system("ln -s $bam\.bai $bamfn\.bai");
+        	if(-e $sjout){
+        		system("cp $sjout .");
+        	}else{
+        		print "(WARNNING) $sjout is not found.\n";
+        	}
         	$bam = $bamfn;
         }
         if($bam=~/\.bam$/){
@@ -165,11 +241,11 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
     }
     
     if($patha == 1){
-    	print "Formatting groupa.txt and groupb.txt...\n";
-    	system("cp groupa.txt groupa.txt.orig");
-    	system("cp groupb.txt groupb.txt.orig");
-    	open(FILE,"groupa.txt.orig") || die "Aborting.. Can't open groupa.txt.orig : $!\n";
-    	open(OUT,">groupa.txt") || die "Aborting.. Can't open groupa.txt : $!\n";
+    	print "Formatting $groupa and $groupb...\n";
+    	system("cp $groupa $groupa.orig");
+    	system("cp $groupb $groupb.orig");
+    	open(FILE,"$groupa.orig") || die "Aborting.. Can't open $groupa.orig : $!\n";
+    	open(OUT,">$groupa") || die "Aborting.. Can't open $groupa : $!\n";
     	while(my $line=<FILE>){
         	chomp $line;
         	next if($line eq "");
@@ -182,8 +258,8 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
         }
         close(OUT);
         close(FILE);
-    	open(FILE,"groupb.txt.orig") || die "Aborting.. Can't open groupb.txt.orig : $!\n";
-    	open(OUT,">groupb.txt") || die "Aborting.. Can't open groupb.txt : $!\n";
+    	open(FILE,"$groupb.orig") || die "Aborting.. Can't open $groupb.orig : $!\n";
+    	open(OUT,">$groupb") || die "Aborting.. Can't open $groupb : $!\n";
     	while(my $line=<FILE>){
         	chomp $line;
         	next if($line eq "");
@@ -312,6 +388,7 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 		print "Getting intron reads....\n";
 		$starttime = time;
 		my $ircount = 0;
+		my @commend;
 		foreach my $bam(keys %group){
 			my $accession = $bam;
 			$accession=~s/\.Aligned\.sortedByCoord\.out\.bam//;
@@ -345,20 +422,32 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 			print "Checking $bam...\n";
 			my $irfn = $accession . ".IR.out.tab";
 			print "Checking $irfn...\n";
-			my $commend = "perl " . $path . "/PSIsigma-ir-v.1.2.pl " . $name . ".db " . $bam . " " . $type;
 			#print "commend = $commend\n";
 			if(-e $irfn){
 				if(-z $irfn){
 					print "Regenerating .IR.out for $accession\n";
-					system("$commend");
+					push(@commend,"perl " . $path . "/PSIsigma-ir-v.1.2.pl " . $name . ".db " . $bam . " " . $type);
+					#$commend .= "perl " . $path . "/PSIsigma-ir-v.1.2.pl " . $name . ".db " . $bam . " " . $type . " &\n";
 				}else{
 					print "$irfn existed. Pass...\n";
 				}
 			}else{
 				print "Generating .IR.out for $accession\n";
-				system("$commend");
+				push(@commend,"perl " . $path . "/PSIsigma-ir-v.1.2.pl " . $name . ".db " . $bam . " " . $type);
+				#$commend .= "perl " . $path . "/PSIsigma-ir-v.1.2.pl " . $name . ".db " . $bam . " " . $type . " &\n";
 			}
 			$ircount++;
+		}
+		if(scalar @commend > 0 && $ircount > 0){
+			my $commend = "";
+			my $commend_count = 0;
+			foreach my $line(@commend){
+				$commend .= "$line &\n";
+				$commend_count++;
+				$commend .= "wait\n" if($commend_count % $threads == 0);
+			}
+			$commend .= "wait\n";
+			system($commend);
 		}
 		$stoptime = time;
 		$hours = sprintf("%.4f",(($stoptime-$starttime)/3600));
@@ -374,7 +463,7 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	$starttime = time;
 	my $adjpdefault = 0;
 	$adjpdefault = 1 if($adjp == 1);
-	my $commend = "perl " . $path . "/PSIsigma-PSI-v.1.1.pl " . $name . ".db " . $name . " " . $supporting_read_criteria . " " . $skipratio . " " . $intron_criteria . " " . $type . " " . $adjpdefault . " " . $denominator . " " . $ircheck . " " . $irrange . " " . $variance;
+	my $commend = "perl " . $path . "/PSIsigma-PSI-v.1.1.pl " . $name . ".db " . $name . " " . $supporting_read_criteria . " " . $skipratio . " " . $intron_criteria . " " . $type . " " . $adjpdefault . " " . $denominator . " " . $ircheck . " " . $irrange . " " . $variance . " " . $groupa . " " . $groupb . " " . $irclean;
 	system($commend);
 	$stoptime = time;
 	$hours = sprintf("%.4f",(($stoptime-$starttime)/3600));
@@ -422,23 +511,54 @@ For commercial purposes, please contact tech transfer office of CSHL via narayan
 	
 	print "\n***Total: $totaltime hours (or " . ($totaltime*60) . "mins).\n";
 	
+	open(LOG,">" . $name . ".Log.txt") || die "Aborting.. Can't open " . $name . ".Log.txt : $!\n";
+	print LOG "Working directory = $path\n";
+	print LOG "Parameters = " . $inputs . "\n";
+	print LOG "Computing time = $totaltime hours (or " . ($totaltime*60) . "mins).\n";
+	close(LOG);
+
+	
+	if($output ne "."){
+		print "Publishing results to $output\n";
+		system("mv $groupa $output/");
+		system("mv $groupb $output/");
+		system("mv $dbname $output/");
+		system("mv $bedname $output/");
+		system("mv " . $gtf . ".mapping.txt $output/");
+		system("mv " . $name . "_r" . $supporting_read_criteria . "_ir" . $intron_criteria . ".txt.tar.gz $output/");
+		system("mv " . $name . "_r" . $supporting_read_criteria . "_ir" . $intron_criteria . ".sorted.txt $output/");
+		system("mv " . $name . ".Log.txt $output/");
+	}
+	
 sub rundb{
 	my $noveljunctioncriteria = shift;
 	my $gtf = shift;
 	my $chrs = shift;
 	my $type = shift;
 	my @chromosomes = split(/\t/,$chrs);
+	my @dbcommend;
 	foreach my $chromosome(@chromosomes){
 		next if($chromosome=~/^chrGL/);
 		next if($chromosome=~/^chrKI/);
 		next if($chromosome=~/^GL/);
 		next if($chromosome=~/^KI/);
 		
-		my $commend = "perl " . $path . "/PSIsigma-db-v.1.0.pl $gtf " . $chromosome . " " . $noveljunctioncriteria . " " . $type . " " . $irmode;
+		#my $commend = "perl " . $path . "/PSIsigma-db-v.1.0.pl $gtf " . $chromosome . " " . $noveljunctioncriteria . " " . $type . " " . $irmode;
+		push(@dbcommend, "perl " . $path . "/PSIsigma-db-v.1.0.pl $gtf " . $chromosome . " " . $noveljunctioncriteria . " " . $type . " " . $irmode . " " . $groupa . " " . $groupb);
 		#print "Doing... $commend\n";
-		print "Doing... $chromosome\n";
-		system("$commend");
+		#print "Doing... $chromosome\n";
+		#system("$commend");
 	}
+	
+	my $commend = "";
+	my $commend_count = 0;
+	foreach my $line(@dbcommend){
+		$commend .= "$line &\n";
+		$commend_count++;
+		$commend .= "wait\n" if($commend_count % $threads == 0);
+	}
+	$commend .= "wait\n";
+	system($commend);
 	
 	system("cat *.db.tmp > $dbname");
 	system("cat *.bed.tmp > $bedname");
@@ -467,6 +587,11 @@ sub param{
 	$parameters{"irrange"} = "-";
 	$parameters{"trimp"} = "-";
 	$parameters{"variance"} = "-";
+	$parameters{"threads"} = "-";
+	$parameters{"groupa"} = "-";
+	$parameters{"groupb"} = "-";
+	$parameters{"output"} = "-";
+	$parameters{"irclean"} = "-";
 	
 	my $oldformat = 1;
 	for(my $i = 0;$i < scalar @array;$i++){
@@ -493,7 +618,7 @@ sub param{
 		return $array[0] . "\t" . $array[1] . "\t" . $array[2] . "\t" . $array[3] . "\t" . "0";
 	}
 	foreach my $key(keys %parameters){
-		next if($key eq "fmode" || $key eq "skipratio" || $key eq "irmode" || $key eq "adjp" || $key eq "denominator" || $key eq "irrange" || $key eq "trimp" || $key eq "variance");
+		next if($key eq "fmode" || $key eq "skipratio" || $key eq "irmode" || $key eq "adjp" || $key eq "denominator" || $key eq "irrange" || $key eq "trimp" || $key eq "variance" || $key eq "groupa" || $key eq "groupb" || $key eq "output" || $key eq "threads" || $key eq "irclean");
 		if($parameters{$key} eq "-"){
 			print "Parameter $key has no input value";
 			return "Parameter $key has no input value";
@@ -505,7 +630,8 @@ sub param{
 	if($parameters{"type"} != 1 && $parameters{"type"} != 2){
 		return "--type parameter should use 1 for short-read or 2 for long-read.";
 	}
-	return $parameters{"gtf"} . "\t" . $parameters{"name"} . "\t" . $parameters{"type"} . "\t" . $parameters{"nread"} . "\t" . $parameters{"fmode"} . "\t" . $parameters{"skipratio"} . "\t" . $parameters{"irmode"} . "\t" . $parameters{"adjp"} . "\t" . $parameters{"trimp"} . "\t" . $parameters{"denominator"} . "\t" . $parameters{"irrange"} . "\t" . $parameters{"variance"};
+	
+	return $parameters{"gtf"} . "\t" . $parameters{"name"} . "\t" . $parameters{"type"} . "\t" . $parameters{"nread"} . "\t" . $parameters{"fmode"} . "\t" . $parameters{"skipratio"} . "\t" . $parameters{"irmode"} . "\t" . $parameters{"adjp"} . "\t" . $parameters{"trimp"} . "\t" . $parameters{"denominator"} . "\t" . $parameters{"irrange"} . "\t" . $parameters{"variance"} . "\t" . $parameters{"threads"} . "\t" . $parameters{"groupa"} . "\t" . $parameters{"groupb"} . "\t" . $parameters{"output"} . "\t" . $parameters{"irclean"};
 }
 
 sub generateSJ{
